a47ee46f917ffebf722f385c12483f113a0ce8a5
Ivaylo Dyakov <ipd3995@gmail.com>
2020-08-12 15:50:40 +0200

    Rake was green...I had to commit

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :address, presence: true
+end
